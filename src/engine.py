6e22d240841e4ef9945558c0c884ef4ce2a258dc
Carter Chamberlain <red7ccx@aol.com>
2017-01-21 03:47:58 -0500

    skeletal generators/factories

--- src/engine.py
+++ src/engine.py
@@ -1,3 +1,6 @@
+from room import RoomFactory
+from thing import ThingFactory
+
 class Engine:
 
     def __init__(self):
@@ -15,10 +18,14 @@ class Engine:
         print(message)
 
     def get_random_thing(self, name, tags=None):
-        pass
+        t_fac = ThingFactory()
+        t_fac.generate_thing()
+        return t_fac.get_thing()
 
     def get_random_room(self, name, tags=None):
-        pass
+        r_fac = RoomFactory()
+        r_fac.generate_room()
+        return r_fac.get_room()
 
     def get_random_attribute(self, name, tags=None):
         pass
