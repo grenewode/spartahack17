9587e77b12605d7a8d40b9a3c07148f0e2871879
Robin Miller <grenewodemiller@gmail.com>
2017-01-21 02:52:15 -0500

    removed attribute code from worldobject, added comments

--- src/worldobject.py
+++ src/worldobject.py
@@ -1,28 +1,43 @@
 class WorldObject:
+    """
+    Describes anything that can be in the game.
+    WorldObjects can have actions
+
+    # Actions
+    Actions are mappings from name -> behavior, which are how the state of the
+    world is changed.
+
+    Also, every WorldObject should set name to some english value, which is used
+    to reference the object in the real world
+    """
     name = None
 
     def __init__(self):
         self.actions = {}
-        self.descriptions = {}
-        self.attributes = {}
 
     def register_action(self, action_name, callback):
+        """
+        Call this function to register an action for this object.
+        """
         self.actions[action_name] = callback
 
     def has_action(self, action, engine):
+        """
+        Allows the game to ast of a world object can do a particular action
+        """
         engine.say("yes" if action in self.actions else "no")
 
     def do_action(self, action, source, engine):
+        """
+        Does an action
+        """
         if action not in self.actions:
             engine.report_error("action not found")
         else:
             self.actions[action](self, source, engine)
-
-    def what_is(self, attribute, engine):
-        if attribute not in self.attributes:
-            engine.report_error("It does not have {} ".format(attribute))
-        else:
-            engine.say(self.attributes[attribute])
-
+            
     def describe(self):
-        return ['this is a ' + self.name]
+        """
+        Called by the world to get a description of the object.
+        """
+        return ['this is a ' + self.name] + [desc for attr in self.attributes for desc in attr.describe()]
