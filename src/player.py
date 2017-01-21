c8a0cedac6b6729b61a868f5e56589ebb1cdefc8
Robin Miller <grenewodemiller@gmail.com>
2017-01-21 17:59:15 -0500

    started fixing player

--- src/player.py
+++ src/player.py
@@ -8,10 +8,14 @@ class Player(WorldObject):
         self.inventory = []
 
         self.register_action('inventory', self.show_inventory)
+        self.register_action('look', self.look)
 
     def show_inventory(self, source, engine):
         engine.say("You have " + ",\n".join([thing.describe()
                                              for thing in self.inventory]))
+        
+    def look(self, source, engine):
+        engine.show_long_description(engine.room.describe())
 
     def add(self, thing):
         self.inventory.append(thing)
