3e37abc8a02117abe175d8c7f99fc90d2d83f660
Zack Bezemek <bezemekz@msu.edu>
2017-01-21 16:03:08 -0500

    zack made a door

---
+++ src/things/door.py
@@ -0,0 +1,22 @@
+class Door():
+    name = "door"
+
+    def __init__(self, engine):
+        super().__init__(engine)
+        self.register_action('open', self.open)
+    def __init__(self, engine):
+        super().__init__(engine)
+        self.register_action('close', self.close)
+    def __init__(self, engine):
+        super().__init__(engine)
+        self.register_action('enter', self.enter)
+
+    def open(self, source, engine):
+        source.open(self)
+
+    def close(self, source, engine):
+        source.close(self)
+
+    def enter(self, source, engine):
+        source.enter(self)
+    
