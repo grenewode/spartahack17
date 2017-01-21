bef9a233d2a5e62363b0eee2ec5827f48f8e3ffd
Zack Bezemek <bezemekz@msu.edu>
2017-01-21 16:46:51 -0500

    blah

--- src/things/door.py
+++ src/things/door.py
@@ -1,12 +1,12 @@
 from attrs.open import Open
-class Door(Things):
+class Door(Thing):
     name = "door"
 
     def __init__(self, engine):
         super().__init__(engine)
         self.register_action('open', self.open)
         self.register_action('close', self.close)
-        self.register_action('enter', self.enter)
+        self.register_action('enter', self.enter) #work on this later, should trigger room generation
 
     def open(self, source, engine):
         self.get_attr(Open).open = True
