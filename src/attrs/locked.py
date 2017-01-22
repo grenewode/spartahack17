901aa4dc25d5136cbe877852f4a35417944c3f85
Zack Bezemek <bezemekz@msu.edu>
2017-01-21 19:07:00 -0500

    cool

--- src/attrs/locked.py
+++ src/attrs/locked.py
@@ -4,10 +4,10 @@ class Locked(Attribute):
 
     def __init__(self):
         super().__init__()
-        self.Locked = False
+        self.locked = False
 
     def describe(self):
-        if self.Locked:
+        if self.locked:
             return "The door is locked."
         else:
-            return "The door is unlocked,"
+            return "The door is unlocked."
